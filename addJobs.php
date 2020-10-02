c8dc88efc4880bfdea7c5449c5ea0aa66304aa93
Micheal Bailey <mbaileyfuturist@gmail.com>
2020-10-01 20:46:43 -0400

    Created addJobs.php so the client can add the jobs they are looking to hire for.

--- addJobs.php
+++ addJobs.php
@@ -1,17 +1,31 @@
-<!DOCTYPE html>
+<?php
+include 'header.php';
+?>
 
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title></title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="">
-    </head>
     <body>
-       
-        
-        <script src="" async defer></script>
+    <h5 class="text-center mt-5 mb-5">Please add the name and description of the jobs you are currently hiring for.</h5>
+
+        <div class="mt-5 ml-5 mr-5 project-container">
+        <form id="initialForm" action="addJobs.php" method="POST">
+
+        <div id="initial-input">
+            <div class="form-group">
+                <label>Job Name</label>
+                <input type="text" class="form-control" name="job_name" id="job_name" placeholder="job name">
+            </div>
+            <div class="form-group">
+                <label>Job Description</label>
+                <textarea type="text" class="form-control" name="job_description" id="job_description" placeholder="job description..." rows="7"></textarea>
+            </div>
+        </div>
+
+            <button type="button" class="btn btn-primary" onclick="addJobs()">Add Job</button>
+            <button type="submit" name="submit" class="btn btn-primary">Done</button>
+        </form>
+        </div>   
     </body>
-</html>
+
+    <script src="js/addJob.js"></script>
+<?php
+    include 'footer.php'
+?>
--- companyInfo.php
+++ companyInfo.php
@@ -46,18 +46,6 @@
         
       }     
 ?>
-
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Company Info</title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="style.css">
-    </head>
-
     <body>
 
         <div class="profile-pic mt-3">
@@ -104,10 +92,6 @@
             </form>
 
         </div>
-        
-    </body>
-</html>
-
 <?php
     include 'footer.php';
 ?>
