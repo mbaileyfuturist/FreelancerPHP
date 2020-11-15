65994bc6ec8a1c6bd0eac3f0624c3546d7bf7608
Micheal Bailey <mbaileyfuturist@gmail.com>
2020-11-14 22:21:44 -0500

    Reading in and displaying the freelancers first name, last name, skill set, hourly pay, bio andservices they provide.

--- FreelancersHomePage.php
+++ FreelancersHomePage.php
@@ -1,8 +1,8 @@
 <?php
 
-  session_start();
-
   include 'header.php';
+
+  session_start();
 
   //Database connection.
   include('config/db_connect.php');
@@ -20,20 +20,18 @@
   }
 
   //Store id's and skill into an array.
-  $client_id = array();
-  $client_skill = array();
   for($index = 0; $index < count($clients_array); $index++){
     $client_id[] = json_encode($clients_array[$index]['id']);
-    $client_skill[] = json_encode($clients_array[$index]['skill']);
+    //$client_skill[] = json_encode($clients_array[$index]['skill']);
+    
     //Remove begining and end quotes.
     $_client_id[] =  trim($client_id[$index], '"');
     $_client_skill[] = trim($client_skill[$index], '"');
   }
   
-  $jobs_array = array();
   for($index = 0; $index < count($client_id); $index++){
     //Grab job_name, job_hourly_rate from jobs where id = $user_id.
-    $job_query = "SELECT job_name, job_hourly_rate, id FROM jobs WHERE id = '$_client_id[$index]'";
+    $job_query = "SELECT job_name, job_description, job_hourly_rate, id FROM jobs WHERE id = '$_client_id[$index]'";
   
     $result2 = mysqli_query($conn, $job_query);
 
@@ -45,11 +43,13 @@
   //Store job names and job hourly rate into arrays.
   for($index = 0; $index < count($jobs_array); $index++){
     $job_names[] = json_encode($jobs_array[$index]['job_name']);
+    $job_descriptions[] = json_encode($jobs_array[$index]['job_description']);
     $job_hourly_rates[] = json_encode($jobs_array[$index]['job_hourly_rate']);
     $job_id[] = json_encode($jobs_array[$index]['id']);
 
     //Remove begining and end quotes.
     $_job_names[] = trim($job_names[$index], '"');
+    $_job_descriptions[] = trim($job_descriptions[$index], '"');
     $_job_hourly_rates[] = trim($job_hourly_rates[$index], '"');
     $_job_id[] = trim($job_id[$index], '"');
   }
@@ -89,7 +89,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-              <a class="nav-link" href="#">Profile <span class="sr-only">(current)</span></a>
+              <a class="nav-link" href="freelancerProfile.php">Profile <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="logout.php">Log Out</a>
@@ -115,10 +115,7 @@
             <tbody>
             <?php 
 
-            //Loop through jobs, if $job_id = $company_id, then create dictionary where 
-            //company_name is the key and the values is an array containing [job_name, skill, job_hourly_rate]
             for($index = 0; $index < count($job_names); $index++){
-        
             $job_id = $_job_id[$index];
             for($index2 = 0; $index2 < count($_company_id); $index2++){
               if($_company_id[$index2] == $job_id){
@@ -128,12 +125,79 @@
                 <td class="text-center"><?php echo $_job_names[$index2]; ?></td>
                 <td class="text-center"><?php echo $_client_skill[$index2]; ?></td>
                 <td class="text-center"><?php echo $_job_hourly_rates[$index2]; ?></td>
+                <td class="text-center"><button type="button" data-toggle="modal" data-target="#myModal" class="btn btn-success" id="select-<?php echo $index?>">Select</button></td>
                 </tr>       
           <?php }
               }
             }?>
             </tbody>
           </table>
+
+          <!-- Modal -->
+          <div class="modal fade" id="myModal" role="dialog">
+            <div class="modal-dialog">
+            
+              <!-- Modal content-->
+              <div class="modal-content">
+                <div class="modal-header">
+                  <button type="button" class="close" data-dismiss="modal">&times;</button>
+                  <h4 class="modal-title">Modal Header</h4>
+                </div>
+                <div class="modal-body">
+                  <p id="job-description"></p>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                </div>
+              </div>
+              
+            </div>
+          </div>
+
+          <script type="text/javascript">
+
+              //Get the number of jobs.
+              var numberOfJobs = "<?php echo count($jobs_array); ?>";
+
+              //Setting job descriptions from php array to js array for later use.
+              var jobDescriptions = <?php echo json_encode($_job_descriptions); ?>;
+
+              var selectBtnElements = []
+              //Array to store the ID's of the job description.
+              var selectButtonIDs = [];
+
+              for(var index = 0; index < numberOfJobs; index++){
+                
+                //Grab the select button element.
+                selectBtnElements.push(document.getElementById('select-' + index));
+
+                //Grab the ID's of the select button elements. 
+                selectBtnID = selectBtnElements[index].getAttribute("id");
+
+                //Add each ID to the array.
+                selectButtonIDs.push(selectBtnID);
+                
+              }
+
+              //Grab ID of selected button.
+              var selectID;
+              $("button").click(function() {
+
+                selectID = this.id;
+
+                //Grab the last character of the ID.
+                var lastCharacter = selectID.slice(-1);
+
+                //Grab the job description at lastCharacter.
+                var jobDescription = jobDescriptions[lastCharacter];
+
+                //Store the job description into the p tags within the modal-body div.
+                var jobDescriptionTags = document.getElementById("job-description");
+                
+                jobDescriptionTags.innerHTML = jobDescription;
+              });
+
+        </script>
 <?php
   include 'footer.php';
 ?>
--- freelancerProfile.php
+++ freelancerProfile.php
@@ -1,5 +1,41 @@
 <?php
   include 'header.php';
+
+  session_start();
+
+  //Database connection.
+  include('config/db_connect.php');
+
+  //Grab the user id from the login screen.
+  $foreign_key = $_SESSION['id'];
+
+  //Retrive the users first_name, last_name.
+  $freelancer_name_query = "SELECT first_name, last_name, skill FROM users WHERE id = '$foreign_key'";
+
+  $freelancer_name_results = mysqli_query($conn, $freelancer_name_query);
+
+  $freelancer_name_associtaive_array = mysqli_fetch_assoc($freelancer_name_results);
+
+  $first_name = $freelancer_name_associtaive_array['first_name'];
+  $last_name = $freelancer_name_associtaive_array['last_name'];
+  $skill = $freelancer_name_associtaive_array['skill'];
+
+  /****************************************************************************************************/
+
+  //Retreive hourly_pay and bio.
+  $freelancer_info_query = "SELECT hourly_pay, bio, services FROM profile_info WHERE id = '$foreign_key'";
+
+  $freelancer_info_results = mysqli_query($conn, $freelancer_info_query);
+
+  $freelancer_info_associtaive_array = mysqli_fetch_assoc($freelancer_info_results);
+
+  $hourly_pay = $freelancer_info_associtaive_array['hourly_pay'];
+  $bio = $freelancer_info_associtaive_array['bio'];
+  $services = $freelancer_info_associtaive_array['services'];
+  
+  if(isset($_POST['submit-1'])){
+    header('Location: FreelancersHomePage.php');
+  }
 ?>
     <body>
     
@@ -7,25 +43,23 @@
             <h2 class="mt-5 text-center text-white">profile pic</h2>
           </div>
     
-          <h4 class="text-center mt-3">Full Name</h4>
-          <h4 class="text-center mt-2">Skill Set</h4>
-          <h4 class="text-center mt-2">Hourly Pay</h4>
+          <h4 class="text-center mt-3"><?php echo $first_name . " " . $last_name; ?></h4>
+          <h4 class="text-center mt-2"><?php echo $skill; ?></h4>
+          <h4 class="text-center mt-2"><?php echo $hourly_pay; ?></h4>
 
           <div class="mt-5" style="width:50%;margin-left:25%;">
-              <h5 class="text-center bio">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum reprehenderit culpa veniam iure cum modi hic eligendi accusantium impedit, dolorem vero quam eveniet ex perspiciatis beatae nihil. Repudiandae, aliquid neque!
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, rem! Enim ex saepe ut facilis aliquid inventore. Ipsa illum, nemo facere cum fugiat nostrum commodi voluptas dignissimos adipisci, sapiente laborum.
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis excepturi ipsa autem mollitia nam a. Consequatur obcaecati perspiciatis illum suscipit ullam impedit velit beatae, quos architecto eligendi accusantium, laborum dolorem.
-              </h5>
-              <h5 class="text-center services-offered">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum reprehenderit culpa veniam iure cum modi hic eligendi accusantium impedit, dolorem vero quam eveniet ex perspiciatis beatae nihil. Repudiandae, aliquid neque!
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, rem! Enim ex saepe ut facilis aliquid inventore. Ipsa illum, nemo facere cum fugiat nostrum commodi voluptas dignissimos adipisci, sapiente laborum.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis excepturi ipsa autem mollitia nam a. Consequatur obcaecati perspiciatis illum suscipit ullam impedit velit beatae, quos architecto eligendi accusantium, laborum dolorem.
-            </h5>
+              <h4 class="text-center">Bio</h6>
+              <h5 class="text-center bio"><?php echo $bio ?></h5></br>
+              <h4 class="text-center">Services</h6>
+              <h5 class="text-center services-offered"><?php echo $services ?></h5>
           </div>
 
-          <div class="mt-5 d-flex justify-content-center">
-            <button class="btn btn-primary mr-3" type="submit">Contact</button>
-            <a href="freelancers.php"><button class="btn btn-primary" type="submit">Done</button></a>
-          </div>
+          <form action="freelancerProfile.php" method="POST">
+            <div class="mt-5 d-flex justify-content-center">
+              <button class="btn btn-primary mr-3" type="submit">Contact</button>
+              <button class="btn btn-primary" type="submit" name="submit-1">Done</button>
+            </div>
+          </form>
 <?php
   include 'footer.php';
 ?>
